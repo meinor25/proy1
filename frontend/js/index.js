0533e3e0c0545b66e776abde039d0a2f2d334ccb
Isaias <isaiasjre2511@gmail.com>
2022-11-27 16:22:08 -0500

    log out function

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -6,5 +6,10 @@ if (!user) {
         ' <a href="./pages/login.html"> <button class="btn-selected">Login</button> </a> <a href="./pages/registrarse.html"> <button class="btn-selected">Register</button></a>';
 } else {
     accountDiv.innerHTML =
-        ' <a href="./pages/profile.html"> <button class="btn-selected">Ver cuenta</button> </a></a>';
+        ' <a href="./pages/profile.html"> <button class="btn-selected">Ver cuenta</button> </a></a> <a href="#"> <button id="log-out" class="btn-selected">log out</button> </a></a>';
+
+    document.querySelector("#log-out").addEventListener("click", () => {
+        localStorage.removeItem("user");
+        window.location.reload();
+    });
 }
