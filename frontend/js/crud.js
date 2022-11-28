c55ee820e901b2e41a278e7227d345b1a6848f39
Isaias <isaiasjre2511@gmail.com>
2022-11-28 18:58:47 -0500

    Proyecto funcionando

--- frontend/js/crud.js
+++ frontend/js/crud.js
@@ -1,32 +1,66 @@
 let baseurl = "http://localhost:8080";
 let consultas = [];
+const button = document.querySelector("#buscar");
+const mostrar = document.querySelector("#mostrar");
+const input = document.querySelector("#search-input");
+let contenedor = document.getElementById("T-Body");
 
 function getConsultas() {
-    fetch(baseurl + "/consultas").then(res=>{
-        res.json().then(json=>{
+    fetch(baseurl + "/consultas").then((res) => {
+        res.json().then((json) => {
+            console.log(json);
             consultas = json;
             ImprimirConsultas();
-        })
-    })
+        });
+    });
 }
 
 function ImprimirConsultas() {
-    let contenedor = document.getElementById("T-Body");
-
-
-    consultas.forEach(({nombre,apellido,fecha_consulta,motivo_consulta}) => {
-        contenedor.innerHTML += CrearConsulta(nombre,apellido,fecha_consulta,motivo_consulta);
-        });
+    consultas.forEach(({ nombre, fecha_consulta, motivo_consulta, nombre_mascota }) => {
+        contenedor.innerHTML += CrearConsulta(
+            nombre,
+            nombre_mascota,
+            fecha_consulta,
+            motivo_consulta
+        );
+    });
 }
 
-function CrearConsulta(nombre,apellido,fecha_consulta,motivo_consulta) {
+function CrearConsulta(nombre, nombre_mascota, fecha_consulta, motivo_consulta) {
     return `<tr>
     <td>${nombre}</td>
-    <td>${apellido}</td>
+    <td>${nombre_mascota}</td>
     <td>${fecha_consulta}</td>
     <td>${motivo_consulta}</td>
-    `
+    `;
 }
 
+function buscarConsulta(value) {
+    fetch(baseurl + "/consultas/nombre", {
+        method: "POST",
+        body: JSON.stringify({ nombre: value }),
+        headers: {
+            "Content-type": "application/json",
+        },
+    }).then((res) => {
+        res.json().then((json) => {
+            consultas = json;
+            ImprimirConsultas();
+        });
+    });
+}
+
+button.addEventListener("click", () => {
+    contenedor.innerHTML = "";
+    buscarConsulta(input.value);
+});
+
+mostrar.addEventListener("click", () => {
+    contenedor.innerHTML = "";
+    getConsultas();
+});
+
 getConsultas();
-setTimeout(()=>{ImprimirConsultas()},1000);
+setTimeout(() => {
+    ImprimirConsultas();
+}, 1000);
